670a7da2858976446cbe98da755c9545fa760961
Manjiri <manjirikshirsagar98@gmail.com>
2026-04-21 17:16:18 +0530

    new method added, commit by manjiri

---
+++ GitJenkins21/src/com/Demo21.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Demo21 {
+	
+	public static void main(String[] args) {
+		System.out.println("this is demo21");
+	}
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+
+}
